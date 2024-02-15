5e9712b0ee1867bdabaaf9676e07de1e1a1787ed
ArmanSiddiqui11 <maanbhai4812@gmail.com>
2024-02-15 11:29:45 +0530

    added nutrition page

--- app/src/main/java/com/example/healthapp/reportsFragment.java
+++ app/src/main/java/com/example/healthapp/reportsFragment.java
@@ -8,12 +8,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link reportsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class reportsFragment extends Fragment {
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -23,6 +26,7 @@ public class reportsFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
 
     public reportsFragment() {
         // Required empty public constructor
@@ -44,6 +48,7 @@ public class reportsFragment extends Fragment {
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
+
     }
 
     @Override
@@ -52,6 +57,7 @@ public class reportsFragment extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
     }
 
@@ -60,5 +66,6 @@ public class reportsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_reports, container, false);
+
     }
 }
